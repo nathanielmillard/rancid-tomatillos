7b27000d149c364ac5564af632bc45f86e94422b
Nathan Darrington <npdarrington@icloud.com>
2020-10-06 15:07:30 -0600

    Modified Navbar to accept a currentUser form App.js and print their name to the screen

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const NavBar = () => {
-  return (
-    <section className='navbar-component'>
-      
-    </section>
-  )
-}
+const NavBar = ({ currentUser }) => {
+	return (
+		<section className='navbar-component'>
+			<h1 className='navbar-title'>Rancid Tomatillos</h1>
+			<button className='navbar-login'>Login/Logout</button>
+			<h3 className='navbar-current-user'>{currentUser}</h3>
+		</section>
+	);
+};
 
 export default NavBar;
