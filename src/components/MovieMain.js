6b462748c750e143024576c40cf0f3be85e844f8
Nathan Darrington <npdarrington@icloud.com>
2020-10-06 16:51:04 -0600

    Imported MovieTile component and built getAllMovieData method to fetch movie data from API and called it with componentDidMount life cycle method

--- src/components/MovieMain.js
+++ src/components/MovieMain.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
 
+import MovieTile from './MovieTile';
+
 class MovieMain extends Component {
   constructor() {
     super();
 
     this.state = {
-
+      movies: []
     }
+  }
+
+  componentDidMount = () => {
+    this.getAllMovieData();
+  }
+
+  getAllMovieData = () => {
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => response.json())
+      .then(data => {
+        console.log('here with componentDidMount')
+        this.setState({movies: data.movies})
+      });
   }
 
   render() {
     return (
       <section className='movie-main'>
-        <h1>Overall Top Rated</h1>
+        {
+          this.state.movies.map(movie => <MovieTile movie={movie} />)
+        }
       </section>
     )
   }
