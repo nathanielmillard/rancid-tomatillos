79e996aff20de03ad1618d1bb74bb36a61a02867
Nathan Darrington <npdarrington@icloud.com>
2020-10-06 14:54:24 -0600

    Changed css import to scss and rendered basic styling for page based on wireframe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
+    <main>
+      {/* Sidebar component goes here */}
+      <div className='sidebar-component'>
+
+      </div>
+      <div className='movie-directory'>
+        <h1>Overall Top Rated</h1>
+      </div>
+      {/* Movie Directory component goes here */}
+    </main>
   );
 }
 
