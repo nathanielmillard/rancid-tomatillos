d96616a15385c2a0ba34fe9e47e65525958f4b16
Nathan Darrington <npdarrington@icloud.com>
2020-10-06 15:16:37 -0600

    Conditionally rendered a user name based on if they are logged in as well as the button showing login or logout based on if a currentUser is present'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,8 +4,10 @@ const NavBar = ({ currentUser }) => {
 	return (
 		<section className='navbar-component'>
 			<h1 className='navbar-title'>Rancid Tomatillos</h1>
-			<button className='navbar-login'>Login/Logout</button>
-			<h3 className='navbar-current-user'>{currentUser}</h3>
+      <button className='navbar-login'>{(!currentUser) ? 'Login' : 'Logout'}</button>
+      {
+        (!currentUser) ? '' : <h3 className='navbar-current-user'>{currentUser}</h3>
+      }
 		</section>
 	);
 };
