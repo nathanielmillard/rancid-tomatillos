b233bb1cfa366f514d5ad94e7fdaaf581cbab3a8
Nathan Darrington <npdarrington@icloud.com>
2020-10-06 15:06:57 -0600

    Converted App.js to a class component with state and gave a currentUser to mimic fetching API data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Navbar from './components/NavBar';
 
 import './App.scss';
 
-function App() {
-  return (
-    <main>
-      {/* Sidebar component goes here */}
-      <Navbar />
-      <div className='movie-directory'>
-        <h1>Overall Top Rated</h1>
-      </div>
-      {/* Movie Directory component goes here */}
-    </main>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: 'Nathaniel'
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        {/* Sidebar component goes here */}
+        <Navbar currentUser={this.state.currentUser} />
+        <div className='movie-directory'>
+          <h1>Overall Top Rated</h1>
+        </div>
+        {/* Movie Directory component goes here */}
+      </main>
+    );
+  }
 }
 
 export default App;
