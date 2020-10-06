0ebf070b2ab67e0adf12cfecb8c0d74f4b105df7
Nathan Darrington <npdarrington@icloud.com>
2020-10-06 15:25:38 -0600

    Set up basic state component for MovieMain

---
+++ src/components/MovieMain.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class MovieMain extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <section className='movie-main'>
+        <h1>Overall Top Rated</h1>
+      </section>
+    )
+  }
+}
+
+export default MovieMain;
