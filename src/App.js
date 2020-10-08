b14019009216d43e9a68b319f65d2f29241f16c6
Nathan Darrington <npdarrington@icloud.com>
2020-10-08 15:45:04 -0600

    Added Redirect to logInUser and logOutUser

--- src/App.js
+++ src/App.js
@@ -3,48 +3,59 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar/NavBar';
 import SignIn from './components/Sign-In/Sign-In';
 import MovieMain from './components/MovieMain/MovieMain';
-import { Route, Switch, Redirect } from 'react-router-dom'
-
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.scss';
 
 class App extends Component {
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      currentUser: '',
-      id: ''
-      //maybe consider Nan lets do research
-    }
-  }
+		this.state = {
+			currentUser: '',
+			id: '',
+			//maybe consider Nan lets do research
+		};
+	}
 
-  logInUser = (user) => {
-    console.log(user)
-    this.setState({currentUser: user.user.name, id: user.user.id})
-    // this.setState(user.user) potential refactor later
-  }
+	logInUser = user => {
+		console.log(user);
+    this.setState({ currentUser: user.user.name, id: user.user.id });
+    return <Redirect to='/' />;
+		// this.setState(user.user) potential refactor later
+	};
 
-  logOutUser = () => {
-    this.setState({currentUser: '', id: ''})
-  }
+	logOutUser = () => {
+    this.setState({ currentUser: '', id: '' });
+    return <Redirect to='/' />;
+  };
+  
+  redirectToHomePage
 
-  render() {
-    return (
-      <main>
-        <Navbar currentUser={this.state.currentUser} signOut = {this.logOutUser} />
-        <Switch>
-          <Route exact path="/" component={MovieMain}/>
-          <Route exact path="/sign-in" render={
-            () => { if (this.state.currentUser === 'Lucy'){
-              return <Redirect to="/"/>
-            } else {
-              return <SignIn logIn={this.logInUser}/>
-            }} }/>
-        </Switch>
-      </main>
-    );
-  }
+	render() {
+		return (
+			<main>
+				<Navbar
+					currentUser={this.state.currentUser}
+					signOut={this.logOutUser}
+				/>
+				<Switch>
+					<Route exact path='/' component={MovieMain} />
+					<Route
+						exact
+						path='/sign-in'
+						render={() => {
+							if (this.state.currentUser === 'Lucy') {
+								return <Redirect to='/' />;
+							} else {
+								return <SignIn logIn={this.logInUser} />;
+							}
+						}}
+					/>
+				</Switch>
+			</main>
+		);
+	}
 }
 
 export default App;
