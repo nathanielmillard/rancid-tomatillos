144bb62a1adafd4b5b50b8d2c361b53b6e9bdea7
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-09 17:37:26 -0600

    Add link around movie tile poster.

--- src/components/MovieTile/MovieTile.js
+++ src/components/MovieTile/MovieTile.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const MovieTile = (props) => {
   return (
     <article className='movie-tile'>
-      <img src={props.movie.poster_path} alt={props.movie.title + " poster"} />
+      <Link to={`/MovieShowPage/${props.movie.id}`}>
+        <img src={props.movie.poster_path} alt={props.movie.title + " poster"}/>
+      </Link>
       <h4 className='movie-title'>Rating: {props.movie.average_rating}</h4>
     </article>
   )
