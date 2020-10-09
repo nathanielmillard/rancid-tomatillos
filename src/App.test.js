01d3be49dcbcb52324a3d82fdb3384e86b46c0de
Nathan Darrington <npdarrington@icloud.com>
2020-10-09 12:48:01 -0600

    Added jest testing for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  // const { getByText } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('Should have default test', () => {
+    expect(true).toBe(true);
+  });
+
+  it('Should render App to the screen', () => {
+    render(<BrowserRouter>
+      <App />
+    </BrowserRouter>);
+  });
+
+  it('Should display a loading message until fetch has finished', () => {
+    render(<BrowserRouter>
+      <App />
+    </BrowserRouter>);
+    expect(screen.getByText('All Movies Trying To Load...')).toBeInTheDocument();
+  });
+
+  // Add additional tests after async testing lesson
+})
