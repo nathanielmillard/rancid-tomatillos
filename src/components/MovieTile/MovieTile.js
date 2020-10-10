4c2f270430124a1d87e5f5579ba4b425abf5eb9a
Nathan Darrington <npdarrington@icloud.com>
2020-10-10 15:06:38 -0600

    Added a new condition render to show a users rating on MovieMain or nothing if their have not rated a movie

--- src/components/MovieTile/MovieTile.js
+++ src/components/MovieTile/MovieTile.js
@@ -2,12 +2,15 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const MovieTile = (props) => {
+  const foundRating = props.userMovieRating.find(rating => rating.movie_id === props.movie.id)
+  if (foundRating) console.log(foundRating);
   return (
     <article className='movie-tile'>
       <Link to={`/MovieShowPage/${props.movie.id}`}>
         <img src={props.movie.poster_path} alt={props.movie.title + " poster"}/>
       </Link>
-      <h4 className='movie-title'>Rating: {props.movie.average_rating}</h4>
+      <h4 className='movie-rating'>Rating: {props.movie.average_rating.toFixed(1)}</h4>
+      { (foundRating) ? <h4 className='movie-user-rating'>Your Rating: {foundRating.rating}</h4> : '' }
     </article>
   )
 }
