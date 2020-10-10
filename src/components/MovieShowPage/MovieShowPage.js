4287b645a02fdaa8bc4d76d11df1d03ef2ae6e36
Nathan Darrington <npdarrington@icloud.com>
2020-10-10 15:06:12 -0600

    Added a new condition render for a user being able to submit a rating or display their rating

--- src/components/MovieShowPage/MovieShowPage.js
+++ src/components/MovieShowPage/MovieShowPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MovieShowPage = (props) => {
-  console.log(props.movie)
+  const foundRating = props.userMovieRating.find(rating => rating.movie_id === props.movie.id)
   return (
   <section className='movie-show-page'>
     <img className='background' src={props.movie.backdrop_path} alt={props.movie.title + 'backdrop'}/>
@@ -11,6 +11,13 @@ const MovieShowPage = (props) => {
         <h1>{props.movie.title}</h1>
         <h2>Release Date: {props.movie.release_date} </h2>
         <h3>Rating: {props.movie.average_rating} </h3>
+        { (foundRating) ? 
+          <h3 className='movie-user-rating'>Your Rating: {foundRating.rating}</h3> : 
+          <label htmlFor='rating'>Rating:
+            <input type='number' value='5' min='1' max='10' />
+            <button>Submit</button>
+          </label>
+        }
         <p>{props.movie.overview}</p>
       </div>
     </div>
