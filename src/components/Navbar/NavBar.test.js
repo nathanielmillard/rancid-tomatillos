7e464607c26913df01a54e44025394090ba00191
Nathan Darrington <npdarrington@icloud.com>
2020-10-15 09:02:48 -0600

    Added test for a user to submit a rating

--- src/components/Navbar/NavBar.test.js
+++ src/components/Navbar/NavBar.test.js
@@ -3,6 +3,8 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom';
 
+import { logInUser } from '../../apiCalls';
+
 import NavBar from './NavBar';
 
 describe("NavBar", () => {
