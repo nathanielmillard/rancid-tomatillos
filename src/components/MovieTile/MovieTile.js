6939c8429b0bb66442cff1d2cd3f7386d6aeb542
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-17 16:44:19 -0600

    Give alt captions and different src for conditional rendering of a favorited movie.

--- src/components/MovieTile/MovieTile.js
+++ src/components/MovieTile/MovieTile.js
@@ -5,12 +5,18 @@ import PropTypes from 'prop-types';
 
 import './MovieTile.scss'
 import favoriteButton from '../../images/favorite-button.svg'
+import unfavoriteButton from '../../images/unfavorite-button.svg'
 
 const MovieTile = (props) => {
   let imagesource = favoriteButton
   let altCaption = 'Favorite this movie'
-  const handleClick = () => { props.toggleFavorite(props.movie) }
+  const handleClick = (e) => { props.toggleFavorite(props.movie, e.target.alt) }
   const foundRating = props.userMovieRatings.find(rating => rating.movie_id === props.movie.id)
+  const foundFavorite = props.userFavorites.find(favorite => favorite === props.movie.id)
+  if(foundFavorite) {
+    altCaption = 'Unfavorite this movie'
+    imagesource = unfavoriteButton
+  }
   return (
     <article className='movie-tile'>
       <Link to={`/MovieShowPage/${props.movie.id}`}>
@@ -31,6 +37,7 @@ export default MovieTile;
 
 MovieTile.propTypes = {
   userMovieRatings: PropTypes.array,
+  userUserFavorites: PropTypes.array,
   movie: PropTypes.object.isRequired,
   toggleFavorite: PropTypes.func
 }
