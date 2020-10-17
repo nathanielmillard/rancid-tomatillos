66f53df87e0a36acdb2d8c1b9cb3af44cd6e5e6a
Nathan Darrington <npdarrington@icloud.com>
2020-10-17 14:26:48 -0600

    Initial structure for CommentsContainer component

---
+++ src/components/CommentsContainer/CommentsContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import Comment from '../Comment/Comment'
+
+export default class CommentsContainer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Comments Container</h1>
+      </>
+    )
+  }
+}
