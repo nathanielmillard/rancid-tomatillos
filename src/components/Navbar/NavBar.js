d2407bc1cf28dba15c37ed28a6748a80206b1cc6
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-17 14:55:51 -0600

    Add buttons to navigate to and from favorites page.

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -10,6 +10,12 @@ const NavBar = (props) => {
 	const signInButton = <Link className='hidden-link' to='/sign-in'><button>SignIn</button>
 	</Link>
 	const signOutButton =  <button onClick={props.signOut}> SignOut </button>
+	const viewFavoritesButton = <Link className='hidden-link' to='/favorites'>
+								<button> View Favorites </button>
+							</Link>
+	const viewAllButton = <Link className='hidden-link' to='/'>
+								<button> View All </button>
+							</Link>
 	return (
 		<section className='navbar-component'>
 			<Link to='/'><h1 className='navbar-title'>Rancid Tomatillos</h1></Link>
@@ -17,6 +23,7 @@ const NavBar = (props) => {
       {
         (!props.currentUser) ? '' : <h3 className='navbar-current-user'>{'Welcome back ' + props.currentUser}</h3>
       }
+			{(props.favoriteView) ? viewAllButton : viewFavoritesButton}
 		</section>
 	);
 };
@@ -26,4 +33,6 @@ export default NavBar;
 NavBar.propTypes = {
 	currentUser: PropTypes.string,
 	signOut: PropTypes.func.isRequired,
+	favoriteView: PropTypes.bool,
+	toggleView: PropTypes.func
 }
