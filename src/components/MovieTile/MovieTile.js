90007b4f6f5e679612b406ac890c95aa8b4a3d13
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-17 16:10:13 -0600

    Add favorite button to movie tiles.

--- src/components/MovieTile/MovieTile.js
+++ src/components/MovieTile/MovieTile.js
@@ -4,15 +4,24 @@ import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types';
 
 import './MovieTile.scss'
+import favoriteButton from '../../images/favorite-button.svg'
 
 const MovieTile = (props) => {
-  const foundRating = props.userMovieRating.find(rating => rating.movie_id === props.movie.id)
+  let imagesource = favoriteButton
+  let altCaption = 'Favorite this movie'
+  const handleClick = () => { props.toggleFavorite(props.movie) }
+  const foundRating = props.userMovieRatings.find(rating => rating.movie_id === props.movie.id)
   return (
     <article className='movie-tile'>
       <Link to={`/MovieShowPage/${props.movie.id}`}>
         <img src={props.movie.poster_path} alt={props.movie.title + " poster"}/>
       </Link>
+      <div className='movie-tile-footer'>
+      <button className='favorite-button' onClick={handleClick}>
+        <img src={imagesource} alt={altCaption}/>
+      </button>
       <h4 className='movie-rating'>Rating: {props.movie.average_rating.toFixed(1)}</h4>
+      </div>
       { (foundRating) ? <h4 className='movie-user-rating'>Your Rating: {foundRating.rating}</h4> : '' }
     </article>
   )
@@ -21,5 +30,7 @@ const MovieTile = (props) => {
 export default MovieTile;
 
 MovieTile.propTypes = {
-  movie: PropTypes.object.isRequired
+  userMovieRatings: PropTypes.array,
+  movie: PropTypes.object.isRequired,
+  toggleFavorite: PropTypes.func
 }
