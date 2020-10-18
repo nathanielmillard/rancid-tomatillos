ee38905bb6f172072c5892f060080c8c524aeb35
Nathan Darrington <npdarrington@icloud.com>
2020-10-17 18:41:44 -0600

    Formatted code into more readable format and added propTypes

--- src/components/CommentsContainer/CommentsContainer.js
+++ src/components/CommentsContainer/CommentsContainer.js
@@ -1,81 +1,92 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 
 import Comment from '../Comment/Comment';
 
 import { getAllMovieComments } from '../../apiCalls';
 
+import './CommentsContainer.scss'
+
 export default class CommentsContainer extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
+		this.state = {
+			comments: [],
+			comment: '',
+			error: '',
+			loading: '',
+		};
+	}
 
-    this.state = {
-      comments: [],
-      comment: '',
-      error: '',
-      loading: ''
-    }
-  }
+	componentDidMount = () => {
+		this.retrieveAllComments();
+	};
 
-  //  get all comments
-  componentDidMount = () => {
-    this.retrieveAllComments();
-  }
+	retrieveAllComments = () => {
+		getAllMovieComments(this.props.movieID).then(response =>
+			this.setState(response)
+		);
+	};
 
-  retrieveAllComments = () => {
-    getAllMovieComments(this.props.movieID)
-      .then(response => this.setState(response));
-  }
+	submitComment = () => {
+		const commentData = {
+			id: Date.now(),
+			movieId: +this.props.movieID,
+			author: this.props.userID,
+			comment: this.state.comment,
+			created_at: Date.now(),
+		};
+		this.setState({ comments: [...this.state.comments, commentData] });
+		this.resetInputs();
+		// this.retrieveAllComments();
+	};
 
-  // submit comment
-  submitComment = () => {
-    const commentData = {
-      id: Date.now(),
-      movieId: +this.props.movieID,
-      author: this.props.userID,
-      comment: this.state.comment,
-      created_at: Date.now()
-    };
-    this.setState({ comments: [...this.state.comments, commentData] });
-    this.resetInputs();
-    // this.retrieveAllComments();
-  }
+	handleChange = event => {
+		const { name, value } = event.target;
+		this.setState({ [name]: value });
+	};
 
-  // updateCommentsState
-  handleChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  }
+	resetInputs = () => {
+		this.setState({ comment: '' });
+	};
 
-  // reset inputs
-  resetInputs = () => {
-    this.setState({ comment: '' });
-  }
+	render() {
+		return (
+			<section className='comments-container'>
+				{!this.props.userID ? (
+					<form className='comment-form'>
+						<label htmlFor='comment'>Submit A New Comment</label>
+						<textarea
+							rows='2'
+							name='comment'
+							value={this.state.comment}
+							onChange={this.handleChange}
+						/>
+						<button type='button' onClick={() => this.submitComment()}>
+							Submit
+						</button>
+					</form>
+				) : (
+					<h2>Login to comment on this movie!</h2>
+				)}
+				{this.state.comments.length < 1 ? (
+					''
+				) : (
+					<section className='comments'>
+						{this.state.comments.map(comment => {
+							return <Comment key={comment.id} comment={comment} />;
+						})}
+					</section>
+				)}
+				{(this.state.error || this.state.loading) && (
+					<h2>{this.state.error || this.state.loading}</h2>
+				)}
+			</section>
+		);
+	}
+}
 
-  render() {
-    return (
-      <>
-        {
-          (!this.props.userID) ?
-          <form>
-            <label htmlFor='comment'>Comment:</label>
-            <textarea rows='2' name='comment' value={this.state.comment} onChange={this.handleChange} />
-            <button type='button' onClick={() => this.submitComment()}>Submit</button>
-          </form> :
-          <h2>Login to comment on this movie!</h2>
-        }
-        {
-          (this.state.comments.length < 1) ? '' 
-          :
-          <section className='comments'>
-            {
-              this.state.comments.map(comment => {
-                return <h1>{comment.created_at}</h1>
-              })
-            }
-          </section>
-        }
-        {(this.state.error || this.state.loading) && <h2>{this.state.error || this.state.loading}</h2>}
-      </>
-    )
-  }
+CommentsContainer.propTypes = {
+	movieID: propTypes.string.isRequired,
+	userID: propTypes.string
 }
