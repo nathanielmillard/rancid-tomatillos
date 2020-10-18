32ff89f64541308520456461c970979f76b1a3b2
Nathan Darrington <npdarrington@icloud.com>
2020-10-17 18:40:09 -0600

    Completed pure Comment component with propTypes

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const Comment = () => {
-  return (
-    <h1>Comment</h1>
-  )
-}
+import propTypes from 'prop-types';
+
+import './Comment.scss';
+
+const Comment = ({ comment }) => {
+	return (
+		<article className='comment'>
+			<section className='comment-header'>
+				<h4 className='comment-title'>Author: {comment.author}</h4>
+				<small className='comment-date'>
+					{new Date(comment.created_at).toLocaleString()}
+				</small>
+			</section>
+			<p className='comment-body'>{comment.comment}</p>
+		</article>
+	);
+};
 
 export default Comment;
+
+Comment.propTypes = {
+  comment: propTypes.object.isRequired
+}
