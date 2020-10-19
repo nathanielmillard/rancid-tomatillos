5305bc13c5a1f4adb8a45f3c5c9a9fce63bfc868
Nathan Darrington <npdarrington@icloud.com>
2020-10-19 16:09:33 -0600

    Added test for Comment component to be rendering a comment to the screen

--- src/components/Comment/Comment.test.js
+++ src/components/Comment/Comment.test.js
@@ -5,3 +5,21 @@ import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 
 import Comment from './Comment';
+
+const mockComment = {
+  id: 101,
+  author: 'Lucy',
+  comment: 'This is a test',
+  created_at: Date.now()
+}
+
+describe('Comment', () => {
+  it.only('Should render a comment to the screen', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <Comment comment={mockComment}/>
+      </MemoryRouter>
+    )
+    expect(getByText('This is a test')).toBeInTheDocument();
+  });
+})
