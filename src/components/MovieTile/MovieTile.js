7c98a3c2b2755575d5ea738d3c1eb23d8f6019eb
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-20 13:59:08 -0600

    Clean up extra lines of code.

--- src/components/MovieTile/MovieTile.js
+++ src/components/MovieTile/MovieTile.js
@@ -5,13 +5,8 @@ import PropTypes from 'prop-types';
 import FavoriteButton from '../FavoriteButton/FavoriteButton';
 
 import './MovieTile.scss'
-import favoriteButton from '../../images/favorite-button.svg'
-import unfavoriteButton from '../../images/unfavorite-button.svg'
 
 const MovieTile = (props) => {
-  let imagesource = favoriteButton
-  let altCaption = 'Favorite this movie'
-  const handleClick = (e) => { props.toggleFavorite(props.movie, e.target.alt) }
   const foundRating = props.userMovieRatings.find(rating => rating.movie_id === props.movie.id)
   const foundFavorite = props.userFavorites.find(favorite => favorite === props.movie.id)
   let isAFavorite = false
