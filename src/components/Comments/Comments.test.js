caa8395ea32160246db1fb9c7ce20d9016240561
Nathan Darrington <npdarrington@icloud.com>
2020-10-19 21:38:33 -0600

    A user should see when a movie has comments logged in or not logged in test

--- src/components/Comments/Comments.test.js
+++ src/components/Comments/Comments.test.js
@@ -14,6 +14,18 @@ const mockNoComments = {
   loading: 'There are no comments for this movie'
 };
 
+const mockMovieComments = {
+  comments: [
+    {
+      "id": 1602971357907,
+      "movieId": 446893,
+      "author": "Lucy",
+      "comment": "This movie is magical!",
+      "created_at": Date.now()
+    }
+  ]
+}
+
 describe('Comments', () => {
   it('Should show that a user has to be logged in to comment on a movie', async () => {
     getAllMovieComments.mockResolvedValueOnce(mockNoComments);
@@ -34,6 +46,17 @@ describe('Comments', () => {
       </MemoryRouter>
     )
     const message = await waitFor(() => getByText('There are no comments for this movie'));
+    expect(message).toBeInTheDocument();
+  });
+
+  it('Should show a user when a movie has comments logged in or not logged in', async () => {
+    getAllMovieComments.mockResolvedValueOnce(mockMovieComments);
+    const { getByText } = render(
+      <MemoryRouter>
+        <Comments movieID={446893} userID={''} />
+      </MemoryRouter>
+    )
+    const message = await waitFor(() => getByText('This movie is magical!'));
     expect(message).toBeInTheDocument();
   });
 })
