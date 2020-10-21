6d0bf08dfde6019cc12b6475d39f4eeb850d31dc
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-20 19:14:51 -0600

    Allow drop shadow so button appears on black back drops.

--- src/components/FavoriteButton/FavoriteButton.js
+++ src/components/FavoriteButton/FavoriteButton.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import './FavoriteButton.scss'
+
 import favoriteButton from '../../images/favorite-button.svg'
 import unfavoriteButton from '../../images/unfavorite-button.svg'
 import { toggleFavoriteMovie } from '../../apiCalls.js'
+
 
 const FavoriteButton = (props) => {
   let imagesource, altCaption
