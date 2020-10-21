923d24cb8bb21678284f99ad140dd1aff856cab1
Nathaniel Millard <nathanielmillard@gmail.com>
2020-10-20 18:42:39 -0600

    Update app tests.

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,11 +3,26 @@ import { MemoryRouter } from 'react-router-dom';
 import { render, screen, waitFor} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import {getAllMovies, getUserFavorites, logInUser, getOneMovie} from '../../apiCalls.js'
+import {getAllMovies, getUserFavorites, logInUser, getOneMovie, getAllMovieComments} from '../../apiCalls.js'
 
 import App from './App';
 
 jest.mock('../../apiCalls.js')
+const trialSingleMovie = {movie: {
+  average_rating: 5.5,
+  backdrop_path: "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+  budget: 0,
+  genres: ["Action"],
+  id: 694919,
+  overview: "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
+  poster_path: "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+  release_date: "2020-09-29",
+  revenue: 0,
+  runtime: 82,
+  tagline: "",
+  title: "Money Plane"
+}}
+
 const trialMovie = {loading: '', movies: [ {
     "id": 694919,
     "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
@@ -21,6 +36,7 @@ describe('App', () => {
   it('Should display NavBar title', async () => {
     getAllMovies.mockResolvedValue(trialMovie)
     getUserFavorites.mockResolvedValue([])
+    getAllMovieComments.mockResolvedValue([])
     logInUser.mockResolvedValue({user: {name: 'Lucy'}})
     render(<MemoryRouter>
       <App />
@@ -56,12 +72,13 @@ describe('App', () => {
   });
 
   it('Should display a movie\'s Show Page after it\'s clicked', async () => {
-    getOneMovie.mockResolvedValue({})
+    getOneMovie.mockResolvedValue(trialSingleMovie)
     render(<MemoryRouter>
       <App />
       </MemoryRouter>);
+    await waitFor(()=> {screen.getByText('Rating: 8.0')})
     userEvent.click(screen.getByAltText('Money Plane poster'))
-    const await waitFor(()=>{ screen.getByText('Money Plane')})
-
+    const movieTitle = await waitFor(()=>{ screen.getByText('Money Plane')})
+    expect(screen.getByText('Rating: 5.5')).toBeInTheDocument();
   });
 })
